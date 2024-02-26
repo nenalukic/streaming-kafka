7aa117dcfef4c8747205e0defe8ff9153eb177e1
Nevenka Lukic <nenamilkov@gmail.com>
2024-02-26 20:33:26 +0100

    docker README file

--- docker/spark/build.sh
+++ docker/spark/build.sh
@@ -7,25 +7,25 @@ JUPYTERLAB_VERSION="3.6.1"
 # -- Building the Images
 
 docker build \
-  -f cluster-base.Dockerfile \
-  -t cluster-base .
+    -f cluster-base.Dockerfile \
+    -t cluster-base .
 
 docker build \
-  --build-arg spark_version="${SPARK_VERSION}" \
-  --build-arg hadoop_version="${HADOOP_VERSION}" \
-  -f spark-base.Dockerfile \
-  -t spark-base .
+    --build-arg spark_version="${SPARK_VERSION}" \
+    --build-arg hadoop_version="${HADOOP_VERSION}" \
+    -f spark-base.Dockerfile \
+    -t spark-base .
 
 docker build \
-  -f spark-master.Dockerfile \
-  -t spark-master .
+    -f spark-master.Dockerfile \
+    -t spark-master .
 
 docker build \
-  -f spark-worker.Dockerfile \
-  -t spark-worker .
+    -f spark-worker.Dockerfile \
+    -t spark-worker .
 
 docker build \
-  --build-arg spark_version="${SPARK_VERSION}" \
-  --build-arg jupyterlab_version="${JUPYTERLAB_VERSION}" \
-  -f jupyterlab.Dockerfile \
-  -t jupyterlab .
+    --build-arg spark_version="${SPARK_VERSION}" \
+    --build-arg jupyterlab_version="${JUPYTERLAB_VERSION}" \
+    -f jupyterlab.Dockerfile \
+    -t jupyterlab .
